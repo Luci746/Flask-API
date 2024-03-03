2f4c2fe764be2576d63eec0a8c74d4e5787f88e0
Luci746 <lucio32bits@gmail.com>
2024-03-03 20:26:04 -0300

    corrigindo commit anterior

---
+++ src/db.py
@@ -0,0 +1,26 @@
+Produtos = [
+    {
+        "id":1,
+        "produto":"Fini Tubes",
+        "price": 3.29,
+        "marca":"Fini"
+    },
+    {
+        "id":2,
+        "produto":"Doritos",
+        "price": 22.99,
+        "marca":"Elma Chips"
+    },
+    {
+        "id":3,
+        "produto":"Ruffles",
+        "price":9.99,
+        "marca":"Elma Chips"
+    },
+    {
+        "id":4,
+        "produto":"Biscoito de Morango",
+        "price":4.59,
+        "marca":"Trakinas"
+    }
+]
